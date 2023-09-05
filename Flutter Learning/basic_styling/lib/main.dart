de6c034c2a0a01a2173d9478ac5c22f3063acaa7
Mishad <mishadcs2002@gmail.com>
2023-09-06 00:15:58 +0600

    how to add backgournd color added

--- Flutter Learning/basic_styling/lib/main.dart
+++ Flutter Learning/basic_styling/lib/main.dart
@@ -6,6 +6,8 @@ void main() {
     const MaterialApp(
       home: Scaffold(
         // We use this scaffold for styling
+        //Now if we wanna give background color to this app,Below the code we have to write
+        backgroundColor: Color.fromRGBO(221, 195, 117, 1),
         body: Center(
           //we got the center by right clicking on text and from refector and got center option
           //And we will see here that Hello World is in center with white background
