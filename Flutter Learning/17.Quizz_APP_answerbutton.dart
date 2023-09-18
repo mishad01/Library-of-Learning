6b79c8b097594bca253321780174966aab12f696
Mishad <mishadcs2002@gmail.com>
2023-09-19 01:41:26 +0600

    update

--- Flutter Learning/17.Quizz_APP_answerbutton.dart
+++ Flutter Learning/17.Quizz_APP_answerbutton.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class answerButton extends StatelessWidget {
-  const answerButton(this.answerText, {super.key});
+  answerButton({super.key, required this.answerText, required this.onTap});
+  // answerButton({
+  //   super.key,
+  //   required this.answerText,
+  //   required this.onTap,
+  // });
   @override
+  void Function() onTap;
   final String answerText;
   Widget build(BuildContext context) {
     return ElevatedButton(
       //To Create Button
-      onPressed: () {},
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 79, 23, 209),
+          foregroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
       child: Text(answerText),
     );
   }
