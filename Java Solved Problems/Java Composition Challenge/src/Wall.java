f78a7a0f509a9686c089d9cb9d0aa21eb21799df
Mishad <mishadcs2002@gmail.com>
2023-09-22 11:52:44 +0600

    final

---
+++ Java Solved Problems/Java Composition Challenge/src/Wall.java
@@ -0,0 +1,9 @@
+public class Wall {
+    String direction;
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+    public String getDirection() {
+        return direction;
+    }
+}
