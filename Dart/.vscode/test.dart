5891375f55c07cbd37e0b8f439fd870e495fccc4
Mishad <mishadcs2002@gmail.com>
2023-11-14 19:20:22 +0600

    dart practice

---
+++ Dart/.vscode/test.dart
@@ -0,0 +1,4 @@
+void main() {
+  var a = stdin.readLineSync();
+  print("Your name $a");
+}
---
+++ Dart/practice.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  //print('Welcome to Dart');
+  var a = stdin.readLineSync();
+  //stdout.write('Input Name ');
+
+  print("My name is $a");
+}
