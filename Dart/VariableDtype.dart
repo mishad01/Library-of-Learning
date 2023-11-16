5295d0dcf0358cc7e7b8acac3bb89a69092f14a5
Mishad <mishadcs2002@gmail.com>
2023-11-16 22:40:11 +0600

    data type

---
+++ Dart/VariableDtype.dart
@@ -0,0 +1,14 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main() {
+  String? number = stdin.readLineSync();
+  int n = int.parse(number!); //Way of taking integer input
+  print(n);
+  //String? b = stdin.readLineSync();
+  String? x = stdin.readLineSync();
+  double c = double.parse(x!);
+  print(c);
+  Char d;
+  Float e;
+}
