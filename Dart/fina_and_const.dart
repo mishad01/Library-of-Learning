ce995a93d54cf9ff6ea1469960568bd3dceb3536
Mishad <mishadcs2002@gmail.com>
2023-11-17 11:52:03 +0600

    final and const

---
+++ Dart/fina_and_const.dart
@@ -0,0 +1,14 @@
+void main() {
+  final name =
+      "Mishad"; //Final is for declaring its the final value we cant change or re assign them
+
+  var test; //Var means we can override as much as possile so we cant use final var
+  final String; //But we can use final in front of Datat type
+
+  //const check; //If we take const then its mendatory to initialize the value
+
+  const cehck2 = "Hello"; //
+
+  final ar = ["hello", "world", "wassup"];
+  ar = ["asef", "saf", "dsfa"]; //Here we can assign new value as we gave final
+}
