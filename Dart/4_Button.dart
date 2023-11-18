273fa161adb9351d016a24cb89bbaaeb574302ae
Mishad <mishadcs2002@gmail.com>
2023-11-18 18:21:56 +0600

    Button

--- Dart/4_Button.dart
+++ Dart/4_Button.dart
@@ -2,31 +2,56 @@
 1. Text Button(Flat Button)
 2. Elevated Button(Raised Button)
 3. Outlined Button*/
-TextButton(
-            onPressed: () {
-              print("On pressed");
-            },
-            onLongPress: () {
-              print("Long pressed");
-            },
-            child: Text("Text Button"),
-          ),
+import 'package:flutter/material.dart';
 
-ElevatedButton(
-            onPressed: () {
-              print("On pressed");
-            },
-            onLongPress: () {
-              print("Long pressed");
-            },
-            child: Text("Text Button"),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  print("On pressed");
+                },
+                onLongPress: () {
+                  print("Long pressed");
+                },
+                child: Text("Text Button"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("On pressed");
+                },
+                onLongPress: () {
+                  print("Long pressed");
+                },
+                child: Text("Text Button"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  print("On pressed");
+                },
+                onLongPress: () {
+                  print("Long pressed");
+                },
+                child: Text("Text Button"),
+              ),
+            ],
           ),
-OutlinedButton(
-            onPressed: () {
-              print("On pressed");
-            },
-            onLongPress: () {
-              print("Long pressed");
-            },
-            child: Text("Text Button"),
-          ),
+        ),
+      ),
+    ),
+  );
+}
