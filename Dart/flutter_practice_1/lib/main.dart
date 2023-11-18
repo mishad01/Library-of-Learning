f127454cfb033256214332019e142472aef9e6e5
Mishad <mishadcs2002@gmail.com>
2023-11-18 23:09:06 +0600

    Inkwell

--- Dart/flutter_practice_1/lib/main.dart
+++ Dart/flutter_practice_1/lib/main.dart
@@ -4,26 +4,18 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter App"),
-        ),
-        body: Container(
-          color: Colors.yellow,
-          height: 300,
-          width: 300,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text("A", style: TextStyle(fontSize: 25)),
-                Text("B", style: TextStyle(fontSize: 25)),
-                Text("C", style: TextStyle(fontSize: 25)),
-                Text("D", style: TextStyle(fontSize: 25)),
-                Text("E", style: TextStyle(fontSize: 25)),
-                ElevatedButton(onPressed: () {}, child: Text("Check"))
-              ]),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Flutter App"),
+          ),
+          body: Center(
+            child: InkWell(
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.amber,
+              ),
+            ),
+          )),
     ),
   );
 }
---
+++ Dart/inkwell.dart
@@ -0,0 +1,8 @@
+/*• InkWell is the material widget in flutter.
+• It responds to the touch action as performed by the user.
+• Inkwell will respond when the user clicks it/
+Tap on it.
+• There are so many gestures like double-tap,
+long press, tap down, etc.*/
+//Mainly if wanna click on image or anything like this 
+//Or we wanna click on image,icon,widget or anything like that we will use Inkwell
