9d1325d9d3c2e3de8dfd74b3b7c2e9b3bb233238
Mishad <mishadcs2002@gmail.com>
2023-11-18 12:33:18 +0600

    Container and center changed

---
+++ Dart/1_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+        body: Container(
+          width: 100,
+          height: 100,
+          color: Colors.blueGrey,
+          child: Text("Hello World"),
+        ),
+      ),
+    ),
+  );
+}
