29207c0a884583daa297bb3f649d645aeb38b1b1
Mishad <mishadcs2002@gmail.com>
2023-11-18 11:50:38 +0600

    Practice Container

---
+++ Dart/1_widget_Container.dart
@@ -0,0 +1,6 @@
+//Container is a invisible Box where can store multiple stuff
+/*It is a widget that combines common painting, positioning, and sizing
+of the child widgets.
+
+It is also a class to store one or more widgets and position them on the
+screen according to our needs.*/
