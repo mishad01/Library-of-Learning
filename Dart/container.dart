fb00eecdef1920f3c1124b2adcc069e116cbca49
Mishad <mishadcs2002@gmail.com>
2023-11-18 12:13:19 +0600

    Container

---
+++ Dart/container.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          width: 100,
+          height: 100,
+          color: Colors.blueGrey,
+          child: Text("Hello World"),
+        ),
+      ),
+    ),
+  );
+}
