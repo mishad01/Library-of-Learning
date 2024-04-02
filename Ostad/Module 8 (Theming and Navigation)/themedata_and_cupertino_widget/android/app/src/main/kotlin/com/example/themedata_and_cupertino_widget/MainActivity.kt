70e77393b4d6bd50fd961daa94ab954d9734c4e2
mishad01 <mishadcs2002@gmail.com>
2024-04-02 13:10:15 +0600

    themedata_and_cupertino_widget

---
+++ Ostad/Module 8 (Theming and Navigation)/themedata_and_cupertino_widget/android/app/src/main/kotlin/com/example/themedata_and_cupertino_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.themedata_and_cupertino_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Ostad/Module 8 (Theming and Navigation)/themedata_and_cupertino_widget/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(iOSStyleApp());
+}
+
+class iOSStyleApp extends StatelessWidget {
+  const iOSStyleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Home'),
+        trailing: CupertinoButton(
+          onPressed: () {},
+          child: Icon(CupertinoIcons.add),
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CupertinoActivityIndicator(
+              radius: 25,
+            ),
+            CupertinoSwitch(
+              value: false,
+              onChanged: (value) {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
