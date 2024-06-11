b8591018ec02f8057dd8d56d0f717d25a90c9079
mishad01 <mishadcs2002@gmail.com>
2024-06-11 12:06:07 +0600

    Update profile UI

--- Ostad/Module 15 (Task Manager)/task_manager/lib/ui/screen/update_profile_screen.dart
+++ Ostad/Module 15 (Task Manager)/task_manager/lib/ui/screen/update_profile_screen.dart
@@ -10,59 +10,140 @@ class UpdateProfileScreen extends StatefulWidget {
 }
 
 class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _mobileTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: profileAppBar(context, true),
       body: BackgroundWidget(
-          child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 25,
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  'Update Profile',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                _buildPhotoPicker(),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _emailTEController,
+                  decoration: InputDecoration(hintText: 'Email'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Input Correct Email';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _firstNameTEController,
+                  decoration: InputDecoration(hintText: 'First Name'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Input First name Correctly';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _lastNameTEController,
+                  decoration: InputDecoration(hintText: 'Last Name'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Input Last Name correctly';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _mobileTEController,
+                  decoration: InputDecoration(hintText: 'Mobile'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Input Mobile Correctly';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: _passwordTEController,
+                  decoration: InputDecoration(hintText: 'Password'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Input Password Correctly';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {}
+                    },
+                    child: Icon(Icons.arrow_forward_ios_rounded)),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
-            Text(
-              'Update Profile',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Container(
-              width: double.maxFinite,
-              height: 48,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8), color: Colors.white),
-              alignment: Alignment.centerLeft,
-              child: Container(
-                width: 100,
-                height: 48,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8), color: Colors.grey),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(),
-          ],
+          ),
         ),
       )),
     );
+  }
+
+  Container _buildPhotoPicker() {
+    return Container(
+      width: double.maxFinite,
+      height: 48,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8), color: Colors.white),
+      alignment: Alignment.centerLeft,
+      child: Container(
+        width: 100,
+        height: 48,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8), color: Colors.grey),
+        alignment: Alignment.center,
+        child: const Text(
+          'Photo',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _emailTEController.dispose();
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _mobileTEController.dispose();
+    _passwordTEController.dispose();
   }
 }
