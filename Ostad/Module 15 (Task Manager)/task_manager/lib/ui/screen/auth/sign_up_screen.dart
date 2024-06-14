01fc3648cdb5da1d8d33fa29aca5ce37ee18ca59
mishad01 <mishadcs2002@gmail.com>
2024-06-14 10:50:43 +0600

    sign up UI update

--- Ostad/Module 15 (Task Manager)/task_manager/lib/ui/screen/auth/sign_up_screen.dart
+++ Ostad/Module 15 (Task Manager)/task_manager/lib/ui/screen/auth/sign_up_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screen/auth/sign_in_screen.dart';
 import 'package:task_manager/ui/utility/app_color.dart';
+import 'package:task_manager/ui/utility/app_constants.dart';
 import 'package:task_manager/ui/widgets/background_widgets.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -17,6 +18,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _firstNameTEController = TextEditingController();
   final TextEditingController _lastNameTEController = TextEditingController();
   final TextEditingController _mobileTEController = TextEditingController();
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,100 +27,137 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(24.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 100,
-                ),
-                Text(
-                  'Join With Us',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(hintText: 'Email'),
-                  controller: _emailTEController,
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-
-                TextFormField(
-                  controller: _firstNameTEController,
-                  decoration: InputDecoration(hintText: 'First Name'),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-
-                TextFormField(
-                  controller: _lastNameTEController,
-                  decoration: InputDecoration(hintText: 'Last Name'),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: _mobileTEController,
-                  decoration: InputDecoration(hintText: 'Mobile'),
-                  keyboardType: TextInputType.phone,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: _passwordTEController,
-                  decoration: InputDecoration(hintText: 'Password'),
-                ),
-
-                const SizedBox(
-                  height: 10,
-                ),
-
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Icon(Icons.arrow_circle_right_outlined),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Center(
-                  child: Column(
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Forgot Password',
-                        ),
-                      ),
-                      RichText(
-                        text: TextSpan(
-                          style: TextStyle(
-                              color: Colors.black.withOpacity(0.8),
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 0.4),
-                          text: "Have account? ",
-                          children: [
-                            TextSpan(
-                              text: 'Sign In',
-                              style: TextStyle(color: AppColor.themeColor),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  _onTapSignInButton();
-                                },
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
+            child: Form(
+              key: _formkey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 100,
                   ),
-                ),
-                //This RichText widget in Flutter is used to display a piece of text with multiple styles in a single text block.
-              ],
+                  Text(
+                    'Join With Us',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(hintText: 'Email'),
+                    controller: _emailTEController,
+                    keyboardType: TextInputType.emailAddress,
+                    //autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your email';
+                      }
+                      if (AppConstants.emailRegex.hasMatch(value!) == false) {
+                        return 'Enter a valid  email Address';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+
+                  TextFormField(
+                    controller: _firstNameTEController,
+                    decoration: InputDecoration(hintText: 'First Name'),
+                    validator: (value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your first name';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+
+                  TextFormField(
+                    controller: _lastNameTEController,
+                    decoration: InputDecoration(hintText: 'Last Name'),
+                    validator: (value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your last name';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _mobileTEController,
+                    decoration: InputDecoration(hintText: 'Mobile'),
+                    keyboardType: TextInputType.phone,
+                    validator: (value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your Mobile Number';
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    controller: _passwordTEController,
+                    decoration: InputDecoration(hintText: 'Password'),
+                    validator: (value) {
+                      if (value?.trim().isEmpty ?? true) {
+                        return 'Enter your password';
+                      }
+                    },
+                  ),
+
+                  const SizedBox(
+                    height: 10,
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {}
+                    },
+                    child: Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Forgot Password',
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                                color: Colors.black.withOpacity(0.8),
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 0.4),
+                            text: "Have account? ",
+                            children: [
+                              TextSpan(
+                                text: 'Sign In',
+                                style: TextStyle(color: AppColor.themeColor),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    _onTapSignInButton();
+                                  },
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  //This RichText widget in Flutter is used to display a piece of text with multiple styles in a single text block.
+                ],
+              ),
             ),
           ),
         ),
