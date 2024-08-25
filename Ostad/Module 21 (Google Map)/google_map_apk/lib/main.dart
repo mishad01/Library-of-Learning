c3c914dec16bc4ece0ac8d53e8a511156a11bc70
Mishad <mishadcs2002@gmail.com>
2024-08-25 16:56:50 +0600

    Google Maps Apk

---
+++ Ostad/Module 21 (Google Map)/google_map_apk/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const GoogleMapsApp());
+}
+
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(22.355231750700757, 91.82074772831926),
+          zoom: 20.4546,
+        ),
+      ),
+    );
+  }
+}
