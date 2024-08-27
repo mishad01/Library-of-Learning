52ddecfd8c5019e4807bd6825968998d0e2925ea
Mishad <mishadcs2002@gmail.com>
2024-08-27 23:28:23 +0600

    2. Real-Time Location Updates

---
+++ Ostad/Assignment/module22_asssignment/android/app/src/main/kotlin/com/example/module22_asssignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.module22_asssignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Ostad/Assignment/module22_asssignment/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:module22_asssignment/app.dart';
+
+void main() {
+  runApp(const MapApp());
+}
