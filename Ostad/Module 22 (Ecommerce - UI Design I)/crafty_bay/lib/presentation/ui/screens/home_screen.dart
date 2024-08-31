3d7de27f3f0849dbb43364af9065664f7b57e4f4
Mishad <mishadcs2002@gmail.com>
2024-09-01 01:34:13 +0600

    Home Screen Update

--- Ostad/Module 22 (Ecommerce - UI Design I)/crafty_bay/lib/presentation/ui/screens/home_screen.dart
+++ Ostad/Module 22 (Ecommerce - UI Design I)/crafty_bay/lib/presentation/ui/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crafty_bay/presentation/ui/utils/app_color.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,7 +12,59 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Home Screen')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                // margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(8.0)),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: const Icon(Icons.search),
+                    hintText: 'Search',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'All Categories',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 1,
+                          fontSize: 15),
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'See All',
+                      style: TextStyle(color: AppColors.themeColor),
+                    ),
+                  )
+                ],
+              )
+              /*const Center(
+                child: Text('Home Screen'),
+              ),*/
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
