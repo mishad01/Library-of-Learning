1a1f46bd1cdbc1e9bde3862fe3c75894aac04d03
Mishad <mishadcs2002@gmail.com>
2024-09-08 11:21:33 +0600

    Home Screen error fixed

--- Ostad/Module 22 (Ecommerce - UI Design I)/crafty_bay/lib/presentation/ui/screens/home_screen.dart
+++ Ostad/Module 22 (Ecommerce - UI Design I)/crafty_bay/lib/presentation/ui/screens/home_screen.dart
@@ -19,49 +19,52 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*Container(
-                    // margin: const EdgeInsets.symmetric(horizontal: 16.0),
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: SearchTextField(
-                        textEditingController: TextEditingController()),
-                  ),*/
-              /*TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.search),
-                      hintText: 'Search',
-                      hintStyle: TextStyle(color: Colors.grey[400]),
-                      fillColor: Colors.grey.shade200,
-                      filled: true,
-                      border: InputBorder.none,
-                      enabledBorder:
-                          InputBorder.none, // Ensures no border when enabled
-                      focusedBorder:
-                          InputBorder.none, // Ensures no border when focused
-                      errorBorder: InputBorder
-                          .none, // Ensures no border when there's an error
-                      disabledBorder: InputBorder.none, // Ensu
-                    ),
-                  ),*/
-              SearchTextField(textEditingController: TextEditingController()),
-              const SizedBox(height: 12),
-              const HomeBannerSlider(),
-              const SizedBox(height: 5),
-              _buildAllCategoriesScreen(),
-              //const SizedBox(height: 10),
-              SectionHeader(title: 'Popular', onTap: () {}),
-              SizedBox(height: 126, child: _buildProductListView()),
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*Container(
+                      // margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: SearchTextField(
+                          textEditingController: TextEditingController()),
+                    ),*/
+                /*TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: const Icon(Icons.search),
+                        hintText: 'Search',
+                        hintStyle: TextStyle(color: Colors.grey[400]),
+                        fillColor: Colors.grey.shade200,
+                        filled: true,
+                        border: InputBorder.none,
+                        enabledBorder:
+                            InputBorder.none, // Ensures no border when enabled
+                        focusedBorder:
+                            InputBorder.none, // Ensures no border when focused
+                        errorBorder: InputBorder
+                            .none, // Ensures no border when there's an error
+                        disabledBorder: InputBorder.none, // Ensu
+                      ),
+                    ),*/
+                const SizedBox(height: 16),
+                SearchTextField(textEditingController: TextEditingController()),
+                const SizedBox(height: 16),
+                const HomeBannerSlider(),
+                const SizedBox(height: 16),
+                _buildAllCategoriesScreen(),
+                const SizedBox(height: 10),
+                SectionHeader(title: 'Popular', onTap: () {}),
+                SizedBox(height: 126, child: _buildProductListView()),
+              ],
+            ),
           ),
         ),
       ),
@@ -109,50 +112,41 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildProductListView() {
-    return SizedBox(
-      height: 160, // Adjust the height to fit your needs
-      child: ListView.separated(
-        separatorBuilder: (context, index) => const SizedBox(width: 12),
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Container(
-            width: 120,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: 120,
-                  height: 100,
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: AppColors.themeColor.withOpacity(0.1),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(8),
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 300,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16),
+                width: 120,
+                height: 80,
+                decoration: BoxDecoration(
+                  color: AppColors.themeColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.only(
                       topRight: Radius.circular(8),
-                    ),
-                    image: const DecorationImage(
+                      topLeft: Radius.circular(8)),
+                  image: const DecorationImage(
                       image: AssetImage(AssetsPath.shoeImage),
-                      fit: BoxFit.scaleDown,
-                    ),
-                  ),
+                      fit: BoxFit.scaleDown),
                 ),
-                const SizedBox(height: 4),
-                const Text(
-                  'Product Name',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 14),
-                ),
-                const SizedBox(height: 4),
-                const Row(
-                  children: [Text('1')],
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+              ),
+              const Text('Product Name',
+                  style: TextStyle(color: AppColors.themeColor)),
+              Row(
+                children: [
+                  const Text('120'),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(width: 10),
     );
   }
 
