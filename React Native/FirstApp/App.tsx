188e2c4e3f694cfe86601a51d2a61a503d0a3028
sakifmishad <109865291+mishad01@users.noreply.github.com>
2026-06-11 19:30:58 +0600

    feat: enhance HomeScreen and FlexScreen layout with improved styling and component structure

--- React Native/FirstApp/App.tsx
+++ React Native/FirstApp/App.tsx
@@ -1,117 +1,168 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Alert, Button, Image, Pressable, StyleSheet, Text, TouchableOpacity, View, ScrollView, Platform } from 'react-native';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { Alert,
+   Button, Image, Pressable, StyleSheet, Text, Touchable, TouchableOpacity, View, ScrollView,Platform,ActivityIndicator} from 'react-native';
+import { SafeAreaView ,SafeAreaProvider} from 'react-native-safe-area-context';
 import FlexScreen from './FlexScreen';
+
 
 const Stack = createNativeStackNavigator();
 
-function App({ navigation }: any) {
-  const onButtonPress = () => Alert.alert('Button component pressed!');
-
+function HomeScreen({ navigation }: { navigation: any }) {
+    const onButtonPress = ()=> Alert.alert('Button component pressed!');
   return (
     <SafeAreaProvider>
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView
+      showsVerticalScrollIndicator = {false}
+     // horizontal = {true}
+      >
         <SafeAreaView style={styles.container}>
-          <Text onPress={() => Alert.alert('Text pressed hahhhahhahhahahahaaa!')} style={styles.text}>
-            Writing our first <Text style={styles.secondText}>"Hello, world!" </Text> app is a celebrated rite of passage in the world of coding.
-          </Text>
+        <Text onPress={() => Alert.alert('Text pressed hahhhahhahhahahahaaa!')}
+        // style = {{
+        //   color: 'black',
+        //   fontSize: 30,
+        //   fontWeight: 'bold',
+        // }}
+        style = {styles.text}
+        >Writing our first <Text style={styles.secondText}>"Hello, world!" </Text> app is a celebrated rite of passage in the world of coding.</Text>
 
-          <Text>{Platform.OS}</Text>
 
-          <Pressable onPress={onButtonPress}>
-            <Image style={styles.imageSize} source={require('./assets/splash-icon.png')} />
-          </Pressable>
+       <Text>{Platform.OS}</Text>
 
-          <Image style={styles.imageSize} source={require('./assets/splash-icon.png')} />
-          <Image style={styles.imageSize} source={require('./assets/splash-icon.png')} />
-          <Image style={styles.imageSize} source={require('./assets/splash-icon.png')} />
-          <Image style={styles.imageSize} source={require('./assets/splash-icon.png')} />
-          <Image style={styles.imageSize} source={require('./assets/splash-icon.png')} />
+       {/* <ActivityIndicator color={'blue'}></ActivityIndicator> */}
+        <Pressable onPress={onButtonPress}>
+          <Image style={styles.imageSize} source ={require('./assets/splash-icon.png')} />
+        </Pressable>
+                  <Image style={styles.imageSize} source ={require('./assets/splash-icon.png')} />
 
-          <TouchableOpacity onPress={onButtonPress}>
-            <Image source={{ uri: 'https://picsum.photos/200/300' }} style={styles.imageSize} />
-          </TouchableOpacity>
+          <Image style={styles.imageSize} source ={require('./assets/splash-icon.png')} />
 
-          <Button title="Press me" onPress={onButtonPress} />
+          <Image style={styles.imageSize} source ={require('./assets/splash-icon.png')} />
 
-          <View style={styles.box} />
+          <Image style={styles.imageSize} source ={require('./assets/splash-icon.png')} />
 
-          <Button title="Go to Flex Screen" onPress={() => navigation.navigate('FlexScreen')} />
+          <Image style={styles.imageSize} source ={require('./assets/splash-icon.png')} />
 
-          <View style={styles.box2}>
-            <View style={styles.childbox2} />
-          </View>
-        </SafeAreaView>
-      </ScrollView>
+
+        <TouchableOpacity onPress={onButtonPress}><Image
+          source={{uri: 'https://picsum.photos/200/300'}}
+          style={styles.imageSize}
+        /></TouchableOpacity>
+
+     <Button title = "Press me" onPress={
+      onButtonPress
+     }>
+
+     </Button>
+
+
+
+     <View style={styles.box}></View>
+
+     <Button title="Press me too" onPress={ ()=>
+      navigation.navigate('FlexScreen')
+     }></Button>
+
+    <View style={styles.box2}>
+      <View style={styles.childbox2}></View>
+    </View>
+      </SafeAreaView></ScrollView>
+          <View style ={{flex:2,backgroundColor: 'tomato'}}></View>
+
     </SafeAreaProvider>
   );
 }
 
-export default function Root() {
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={App} />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="FlexScreen" component={FlexScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+
+
+/*
+StyleSheet.create = Create method is taking an object as an argument and returning an object
+with the same keys but with the values transformed to a style object that can be used in the*/
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, //How much space the container should take up
+   // height: 100, //Height of the container
     backgroundColor: 'rgb(159, 159, 159)',
-    padding: 10,
+    padding:10,
+   // alignItems: 'center',
+   // justifyContent: 'center',
   },
-  text: {
+
+  text:{
     color: 'black',
     fontSize: 30,
     fontWeight: 'bold',
   },
-  secondText: {
+
+  secondText:{
     color: 'blue',
   },
-  box: {
+
+  box : {
     height: 100,
     width: 100,
     backgroundColor: 'skyblue',
     borderRadius: 10,
     shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5,
+    elevation: 5, //for android
   },
-  box2: {
+
+  box2 : {
     height: 100,
     width: 100,
     backgroundColor: 'skyblue',
     borderRadius: 10,
     shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5,
+    elevation: 5, //for android
     marginTop: 10,
   },
-  childbox2: {
+
+  childbox2 : {
     height: 50,
     width: 50,
     backgroundColor: 'steelblue',
     borderRadius: 10,
     shadowColor: 'black',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5,
+    elevation: 5, //for android
     marginTop: 25,
+    marginLeft: 0,
+    
   },
-  imageSize: {
+
+  imageSize:{
     height: 100,
     width: 100,
     alignSelf: 'center',
     marginTop: 20,
-  },
+
+  }
 });
--- React Native/FirstApp/FlexScreen.tsx
+++ React Native/FirstApp/FlexScreen.tsx
@@ -1,15 +1,31 @@
-import { View,StyleSheet } from "react-native";
+import { View,StyleSheet, Text } from "react-native";
 
 export default function FlexScreen() {
     return (
-        <View style = {{flex:1}}>
-            <View style = {{flex:1,backgroundColor: 'powderblue'}}></View>
+        <View style = {{flex:1,flexDirection: "row",
+          backgroundColor: 'powderblue',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        alignItems: 'center'}}>
+            <View style = {{flex:1,backgroundColor: 'powderblue',flexDirection: "row"}}>
+              <View style={styles.box}></View>
+              <View style={styles.box}></View>
+              <View style={styles.box}></View>
+              <View style={styles.box}></View>
+              <View style={styles.box}></View>
+              <View style={styles.box}></View>
+
+            </View>
             <View style = {{flex:2,backgroundColor: 'skyblue'}}></View>
             <View style = {{flex:3,backgroundColor: 'steelblue'}}></View>
         </View>
     );
 }
 
+
+/*
+StyleSheet.create = Create method is taking an object as an argument and returning an object
+with the same keys but with the values transformed to a style object that can be used in the*/
 const styles = StyleSheet.create({
   container: {
     flex: 1, //How much space the container should take up
@@ -24,4 +40,71 @@ const styles = StyleSheet.create({
     color: 'black',
     fontSize: 30,
     fontWeight: 'bold',
-  },});
+  },
+
+  secondText:{
+    color: 'blue',
+  },
+
+  box : {
+    height: 100,
+    width: 100,
+    backgroundColor: 'skyblue',
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, //for android
+  },
+
+  box2 : {
+    height: 100,
+    width: 100,
+    backgroundColor: 'skyblue',
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, //for android
+    marginTop: 10,
+  },
+
+  childbox2 : {
+    height: 50,
+    width: 50,
+    backgroundColor: 'steelblue',
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, //for android
+    marginTop: 25,
+    marginLeft: 0,
+    color: 'white',
+    left: 10,
+    right: 20,
+
+    
+  },
+
+
+  imageSize:{
+    height: 100,
+    width: 100,
+    alignSelf: 'center',
+    marginTop: 20,
+
+  }
+});
