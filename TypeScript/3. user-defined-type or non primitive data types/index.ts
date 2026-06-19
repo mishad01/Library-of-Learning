fb326f67a0aa4ae532eefbcb0bae826d8e1c1d3f
sakifmishad <109865291+mishad01@users.noreply.github.com>
2026-06-19 12:21:43 +0600

    feat: add TypeScript files for user-defined types and implement union type functionality

---
+++ TypeScript/3. user-defined-type or non primitive data types/index.ts
@@ -0,0 +1,34 @@
+// 1. union ( | ): 
+// Union Type - more than one type for variable or function parameter.
+// Program - odd/even for number and string to give idea about this union concept.
+let userId = 'Mishad';
+
+function userIdDataType (userId: string | number){
+    console.log(typeof userId)
+}
+
+userIdDataType('123'); 
+userIdDataType(123); 
+
+//Arrow fucntion
+const isEven = (num : number | string )=>{
+    if(typeof(num)==='number'){
+        if(num%2===0){
+            return 'Even'
+        }else{
+            return 'Odd'
+        }
+    }else{
+        num = Number(num)
+        if(num%2===0){
+            return 'Even'
+        }else{
+            return 'Odd'
+        } 
+    }
+}
+
+console.log(isEven(5));
+console.log(isEven(10));
+console.log(isEven("8"))
+console.log(isEven("12"))
